b780c4eac30a40bbdd7a945a74f88c8a18c0a672
Vladimir Roncevic <elektron.ronca@gmail.com>
2025-11-27 22:08:50 +0100

    [gen_readme] Updated code coverage, version, docs

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # @brief   gen_readme
-# @version v1.1.3
+# @version v1.1.4
 # @date    Sat Aug 1 07:52:38 2020
 # @company None, free software to use 2020
 # @author  Vladimir Roncevic <elektron.ronca@gmail.com>
@@ -9,10 +9,11 @@
 
 rm -rf htmlcov gen_readme_coverage.xml gen_readme_coverage.json .coverage
 rm -rf fresh_new/ full_simple_new/ latest_pro/ simple_read/ simple_write/
-ats_coverage_run.py -n gen_readme -p ../README.md
-rm -rf fresh_new/ full_simple_new/ latest_pro/ simple_read/ simple_write/
 python3 -m coverage run -m --source=../gen_readme unittest discover -s ./ -p '*_test.py' -vvv
 python3 -m coverage html -d htmlcov
 python3 -m coverage xml -o gen_readme_coverage.xml 
 python3 -m coverage json -o gen_readme_coverage.json
 python3 -m coverage report --format=markdown -m
+python3 ats_coverage.py -n gen_readme
+rm htmlcov/.gitignore
+echo "Done"
